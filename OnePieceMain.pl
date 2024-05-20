855698e913fa24ea63ad7beabaa6080cb45e0136
cemanuelsilva <carlosdacunhapro@gmail.com>
2024-05-20 17:07:09 +0100

    Update

--- OnePieceMain.pl
+++ OnePieceMain.pl
@@ -1,66 +1,115 @@
 % Facts about One Piece characters
 % Straw_hats_Pirates
-character(luffy, captain, straw_hats, [rubber_fruit]).
-character(zoro, swordsman, straw_hats, []).
-character(nami, navigator, straw_hats, []).
-character(usopp, sniper, straw_hats, []).
-character(sanji, cook, straw_hats, [black_leg_style]).
-character(chopper, doctor, straw_hats, [human_human_fruit]).
-character(robin, archaeologist, straw_hats, [hana_hana_fruit]).
-character(franky, shipwright, straw_hats, []).
-character(brook, musician, straw_hats, [soul_soul_fruit]).
+character(luffy, captain, straw_hats, [rubber_fruit], 3_000_000_000).
+character(zoro, swordsman, straw_hats, [], 1_111_000_000).
+character(nami, navigator, straw_hats, [], 366_000_000).
+character(usopp, sniper, straw_hats, [], 500_000_000).
+character(sanji, cook, straw_hats, [black_leg_style], 1_032_000_000).
+character(chopper, doctor, straw_hats, [human_human_fruit], 1_000).
+character(robin, archaeologist, straw_hats, [hana_hana_fruit], 930_000_000).
+character(franky, shipwright, straw_hats, [], 394_000_000).
+character(brook, musician, straw_hats, [soul_soul_fruit], 383_000_000).
 
 %Blackbeard_Pirates
-character(marshall_d_teach, captain, blackbeard, [dark_dark_fruit, tremor_tremor_fruit]).
-character(laffitte, chief, blackbeard, [unconfirmed]).
-character(van_augur, sniper, blackbeard, [warp_warp_fruit]).
-character(doc_Q, doctor ,blackbeard, [sick_sick_fruit]).
-character(jesus_burgess, helmsman, blackbeard, [dark_dark_fruit]).
-character(shiryu, swordman, blackbeard, [clear_clear_fruit]).
-character(avalo_pizarro, titanic, blackbeard, [isle_isle_fruit]).
-character(vasco_shot, titanic, blackbeard, [glug_glug_fruit]).
-character(catarina_devon, titanic, blackbeard, [dog_dog_fruit]).
-character(kuzan ,titanic, blackbeard, [ice_ice_fruit]).
-character(san_juan_wold ,titanic, blackbeard, [huge_huge_fruit]).
+character(marshall_d_teach, captain, blackbeard, [dark_dark_fruit, tremor_tremor_fruit], 3_996_000_000).
+character(laffitte, chief, blackbeard, [unconfirmed], 422_000_000).
+character(van_augur, sniper, blackbeard, [warp_warp_fruit], 64_000_000).
+character(doc_q, doctor, blackbeard, [sick_sick_fruit], 72_000_000).
+character(jesus_burgess, helmsman, blackbeard, [dark_dark_fruit], 20_000_000).
+character(shiryu, swordsman, blackbeard, [clear_clear_fruit], 0).  % Current bounty unknown
+character(avalo_pizarro, titanic, blackbeard, [isle_isle_fruit], 0).  % Current bounty unknown
+character(vasco_shot, titanic, blackbeard, [glug_glug_fruit], 0).  % Current bounty unknown
+character(catarina_devon, titanic, blackbeard, [dog_dog_fruit], 0).  % Current bounty unknown
+character(kuzan, titanic, blackbeard, [ice_ice_fruit], 0).  % Former Marine Admiral
+character(san_juan_wolf, titanic, blackbeard, [huge_huge_fruit], 0).  % Current bounty unknown
 
 %Red_Haired_Pirates
-character(shanks, captain, red_haired_pirates, []).
-character(benn_beckman, first_mate, red_haired_pirates, []).
-character(lucky_roux, member, red_haired_pirates, []).
-character(yasopp, sniper, red_haired_pirates, []).
-character(rockstar, member, red_haired_pirates, []).
+character(shanks, captain, red_haired_pirates, [], 4_048_900_000).
+character(benn_beckman, first_mate, red_haired_pirates, [], 0).  % Current bounty unknown
+character(lucky_roux, member, red_haired_pirates, [], 0).  % Current bounty unknown
+character(yasopp, sniper, red_haired_pirates, [], 0).  % Current bounty unknown
+character(rockstar, member, red_haired_pirates, [], 94_000_000).
 
 %Whitebeard_Pirates
-character(whitebeard, captain, whitebeard_pirates, [gura_gura_fruit]).
-character(marco, first_division_commander, whitebeard_pirates, [phoenix_fruit]).
-character(portgas_d_ace, second_division_commander, whitebeard_pirates, [flame_flame_fruit]).
-character(jozu, third_division_commander, whitebeard_pirates, [diamond_fruit]).
-character(vista, fourth_division_commander, whitebeard_pirates, []).
-character(thatch, fifth_division_commander, whitebeard_pirates, []).
+character(whitebeard, captain, whitebeard_pirates, [gura_gura_fruit], 5_046_000_000).
+character(marco, commander, whitebeard_pirates, [phoenix_fruit], 1_374_000_000).
+character(portgas_d_ace, second_division_commander, whitebeard_pirates, [flame_flame_fruit], 550_000_000).
+character(jozu, commander, whitebeard_pirates, [diamond_fruit], 0).
+character(vista, commander, whitebeard_pirates, [], 0).
+character(thatch, commander, whitebeard_pirates, [], 0).
+character(blamenco, commander, whitebeard_pirates, [], 0).
+character(rakuyo, commander, whitebeard_pirates, [], 0).
+character(namur, commander, whitebeard_pirates, [], 0).
+character(blenheim, commander, whitebeard_pirates, [], 0).
+character(curiel, commander, whitebeard_pirates, [], 0).
+character(kingdew, commander, whitebeard_pirates, [], 0).
+character(haruta, commander, whitebeard_pirates, [], 0).
+character(atomos, commander, whitebeard_pirates, [], 0).
+character(speed_jiru, commander, whitebeard_pirates, [], 0).
+character(fossa, commander, whitebeard_pirates, [], 0).
+character(izo, commander, whitebeard_pirates, [], 510_000_000).
 
-%Buggy_Pirates
-character(buggy, captain, buggy_pirates, [bara_bara_fruit]).
-character(mohji, member, buggy_pirates, []).
-character(cabaji, member, buggy_pirates, []).
-character(alvida, member, buggy_pirates, [sube_sube_fruit]).
+%Cross_Guild
+character(buggy, captain, cross_guild, [bara_bara_fruit], 3_189_000_000).
+character(mihawk, member, cross_guild, [], 3_590_000_000).
+character(crocodile, member, cross_guild, [suna_suna_fruit], 1_965_000_000).
+character(mohji, member, cross_guild, [], 0).
+character(cabaji, member, cross_guild, [], 0).
+character(alvida, member, cross_guild, [sube_sube_fruit], 5_000_000).
+
+% Big_Mom_Pirates
+character(charlotte_lilin, captain, big_mom_pirates, [soru_soru_fruit], 4_388_000_000).
+character(charlotte_katakuri, sweet_commander, big_mom_pirates, [mochi_mochi_fruit], 1_057_000_000).
+character(charlotte_smoothie, sweet_commander, big_mom_pirates, [shibo_shibo_fruit], 932_000_000).
+character(charlotte_cracker, sweet_commander, big_mom_pirates, [bisu_bisu_fruit], 860_000_000).
+character(charlotte_perospero, minister, big_mom_pirates, [pero_pero_fruit], 700_000_000).
+character(charlotte_snack, sweet_commander, big_mom_pirates, [], 600_000_000).
+character(charlotte_oven, minister, big_mom_pirates, [netsu_netsu_fruit], 300_000_000).
+character(charlotte_daifuku, minister, big_mom_pirates, [hoya_hoya_fruit], 300_000_000).
+character(charlotte_compote, minister, big_mom_pirates, [], 0).
+character(charlotte_mont-dor, minister, big_mom_pirates, [buku_buku_fruit], 120_000_000).
+character(charlotte_galette, minister, big_mom_pirates, [bata_bata_fruit], 0).
+character(charlotte_broyé, minister, big_mom_pirates, [], 0).
+character(charlotte_bavarois, minister, big_mom_pirates, [], 0).
+character(charlotte_pudding, minister, big_mom_pirates, [mema_mema_fruit], 0).
+character(zeus, homie, big_mom_pirates, [], 0).
+character(prometheus, homie, big_mom_pirates, [], 0).
+character(napoleon, homie, big_mom_pirates, [], 0).
+
+%Beast_Pirates
+character(kaido, captain, beast_pirates, [dragon_fruit], 4_611_100_000).
+character(king, all_star, beast_pirates, [lunarian], 1_390_000_000).
+character(queen, all_star, beast_pirates, [brachiosaurus_fruit], 1_320_000_000).
+character(jack, all_star, beast_pirates, [mammoth_fruit], 1_000_000_000).
+character(x_drake, tobiroppo, beast_pirates, [allosaurus_fruit], 222_000_000).
+character(page_one, tobiroppo, beast_pirates, [spinosaurus_fruit], 290_000_000).
+character(ulti, tobiroppo, beast_pirates, [pachycephalosaurus_fruit], 400_000_000).
+character(sasaki, tobiroppo, beast_pirates, [triceratops_fruit], 472_000_000).
+character(black_maria, tobiroppo, beast_pirates, [ancient_spider_fruit], 480_000_000).
+character(who_is_who, tobiroppo, beast_pirates, [saber_tiger_fruit], 546_000_000).
+character(sheepshead, headliner, beast_pirates, [sheep_sheep_fruit], 0).
+character(ginrummy, headliner, beast_pirates, [], 0).
+character(holdem, headliner, beast_pirates, [lion_smile_fruit], 0).
+character(speed, headliner, beast_pirates, [horse_smile_fruit], 0).
+character(babanuki, headliner, beast_pirates, [elephant_smile_fruit], 0).
 
 
 % Predicate to check if a character is a captain
 is_captain(Character) :-
-    character(Character, captain, _, _).
+    character(Character, captain, _, _,_).
 
 % Predicate to check if a character is from a specific crew
 is_from_crew(Character, Crew) :-
-    character(Character, _, Crew, _).
+    character(Character, _, Crew, _,_).
 
 % Predicate to check if a character has a specific Devil Fruit
 has_devil_fruit(Character, Fruit) :-
-    character(Character, _, _, Fruits),
+    character(Character, _, _, Fruits,_),
     member(Fruit, Fruits).
 
 % Predicate to show all characters from a crew
 show_characters_from_crew(Crew) :-
-    character(Character, _, Crew, _),
+    character(Character, _, Crew, _,_),
     write(Character),
     nl,
     fail. % Backtrack to find more characters
@@ -68,36 +117,71 @@ show_characters_from_crew(_). % End of search
 
 % Predicate to get the crew of a character
 get_crew(Character) :-
-    character(Character, _, Crew, _),
+    character(Character, _, Crew, _,_),
     writeln(Crew).
 
 % Predicate to get the role of a character
 get_role(Character) :-
-    character(Character, Role, _, _),
+    character(Character, Role, _, _,_),
     writeln(Role).
 
 % Predicate to get the Devil Fruit of a character
 get_devil_fruit(Character) :-
-    character(Character, _, _, Fruits),
+    character(Character, _, _, Fruits,_),
     writeln(Fruits).
+
+% Predicate to get the bounty of a character
+get_bounty(Character) :-
+    character(Character, _, _, _, Bounty),
+    writeln(Bounty).
+
+% Predicate to show all roles
+show_all_roles :-
+    findall(Role, character(_, Role, _, _, _), Roles),
+    sort(Roles, UniqueRoles),
+    writeln('All roles:'),
+    forall(member(Role, UniqueRoles), format("- ~s~n", [Role])).
+
+show_all_crews :-
+    findall(Crew, character(_, _, Crew, _, _), Crews),
+    sort(Crews, UniqueCrews),
+    writeln('All crews:'),
+    forall(member(Crew, UniqueCrews), format("- ~s~n", [Crew])).
+
+% Predicate to display a top 10 list of the highest bounties
+top_10_bounties :-
+    setof(Bounty-Character, Crew^Role^DevilFruits^character(Character, Role, Crew, DevilFruits, Bounty), Bounties),
+    reverse(Bounties, ReversedBounties),
+    take(10, ReversedBounties, Top10),
+    forall(member(Bounty-Character, Top10), format("~s: ~d~n", [Character, Bounty])).
+
+take(0, _, []).
+take(N, [H|T], [H|R]) :-
+    N > 0,
+    N1 is N - 1,
+    take(N1, T, R).
+
 
 
 % Menu for interacting with the Prolog database
 
 % Main menu
 main_menu :-
-    writeln('1. Check if a character is a captain'),
-    writeln('2. Check if a character is from a specific crew'),
-    writeln('3. Check if a character has a specific Devil Fruit'),
-    writeln('4. Show all characters from a crew'),
-    writeln('5. Get the crew of a character'),
-    writeln('6. Get the role of a character'),
-    writeln('7. Get the Devil Fruit of a character'),
-    writeln('8. Get the Details of a character'),
-    writeln('9. Show all characters with a role'),
-    writeln('10. Exit'),
-    nl,
-    write('Insert your choice (1-10): '),
+    writeln('1.  Check if a character is a captain'),
+    writeln('2.  Check if a character is from a specific crew'),
+    writeln('3.  Check if a character has a specific Devil Fruit'),
+    writeln('4.  Get the crew of a character'),
+    writeln('5.  Get the role of a character'),
+    writeln('6.  Get the Devil Fruit of a character'),
+    writeln('7.  Get the bounty of a character'),
+    writeln('8.  Get the Details of a character'),
+    writeln('9.  Show all characters with a role'),
+    writeln('10. Show all crews'),
+    writeln('11. Show all characters from a crew'),
+    writeln('12. Show all roles'),
+    writeln('13. Show top 10 highest bounties'),
+    writeln('14. Exit'),
+    write('Insert your choice (1-14): '),
     read_choice.
 
 % Read choice and process input
@@ -109,13 +193,16 @@ read_choice :-
 
 % Process user input
 process_input(1) :-
+    nl,
     writeln('Enter the character name: '),
     read_line_to_codes(user_input, CharacterCodes),
     atom_codes(CharacterAtom, CharacterCodes),
     process_captain(CharacterAtom),
     nl,
     main_menu.
+
 process_input(2) :-
+    nl,
     writeln('Enter the character name: '),
     read_line_to_codes(user_input, CharacterCodes),
     atom_codes(CharacterAtom, CharacterCodes),
@@ -125,7 +212,9 @@ process_input(2) :-
     process_crew(CharacterAtom, CrewAtom),
     nl,
     main_menu.
+
 process_input(3) :-
+    nl,
     writeln('Enter the character name: '),
     read_line_to_codes(user_input, CharacterCodes),
     atom_codes(CharacterAtom, CharacterCodes),
@@ -135,35 +224,45 @@ process_input(3) :-
     process_fruit(CharacterAtom, FruitAtom),
     nl,
     main_menu.
-process_input(4) :-
+
+process_input(11) :-
+    nl,
     writeln('Enter the crew name: '),
     read_line_to_codes(user_input, CrewCodes),
     atom_codes(CrewAtom, CrewCodes),
     process_show_characters_from_crew(CrewAtom),
     nl,
     main_menu.
-process_input(5) :-
+
+process_input(4) :-
+    nl,
     writeln('Enter the character name: '),
     read_line_to_codes(user_input, CharacterCodes),
     atom_codes(CharacterAtom, CharacterCodes),
     process_get_crew(CharacterAtom),
     nl,
     main_menu.
-process_input(6) :-
+
+process_input(5) :-
+    nl,
     writeln('Enter the character name: '),
     read_line_to_codes(user_input, CharacterCodes),
     atom_codes(CharacterAtom, CharacterCodes),
     process_get_role(CharacterAtom),
     nl,
     main_menu.
-process_input(7) :-
+
+process_input(6) :-
+    nl,
     writeln('Enter the character name: '),
     read_line_to_codes(user_input, CharacterCodes),
     atom_codes(CharacterAtom, CharacterCodes),
     process_get_devil_fruit(CharacterAtom),
     nl,
     main_menu.
+
 process_input(8) :-
+    nl,
     writeln('Enter the character name: '),
     read_line_to_codes(user_input, CharacterCodes),
     atom_codes(CharacterAtom, CharacterCodes),
@@ -171,14 +270,45 @@ process_input(8) :-
     search_character_info(CharacterAtom),
     nl,
     main_menu.
+
 process_input(9) :-
+    nl,
     writeln('Enter the role name (e.g., captain, sniper, doctor): '),
     read_line_to_codes(user_input, RoleCodes),
     atom_codes(RoleAtom, RoleCodes),
     find_characters_with_role(RoleAtom),
     nl,
     main_menu.
+
 process_input(10) :-
+    nl,
+    show_all_crews,
+    nl,
+    main_menu.
+
+process_input(7) :-
+    nl,
+    writeln('Enter the character name: '),
+    read_line_to_codes(user_input, CharacterCodes),
+    atom_codes(CharacterAtom, CharacterCodes),
+    process_get_bounty(CharacterAtom),
+    nl,
+    main_menu.
+
+process_input(12) :-
+    nl,
+    show_all_roles,
+    nl,
+    main_menu.
+
+process_input(13) :-
+    nl,
+    writeln('Top 10 highest bounties:'),
+    top_10_bounties,
+    nl,
+    main_menu.
+process_input(14) :-
+    nl,
     writeln('Exiting...').
 
 % Predicate to process captain query
@@ -192,6 +322,10 @@ process_crew(Character, Crew) :-
 % Predicate to process devil fruit query
 process_fruit(Character, Fruit) :-
     (has_devil_fruit(Character, Fruit) -> writeln('Yes, the character has the specified Devil Fruit.'); writeln('No, the character does not have the specified Devil Fruit.')).
+
+process_get_bounty(Character) :-
+    writeln('Bounty of the character:'),
+    get_bounty(Character).
 
 % Predicate to process showing characters from a crew query
 process_show_characters_from_crew(Crew) :-
@@ -215,7 +349,7 @@ process_get_devil_fruit(Character) :-
 
 % Predicate to find characters with a specific role
 find_characters_with_role(Role) :-
-    character(Character, Role, _, _),
+    character(Character, Role, _, _,_),
     write(Character),
     nl,
     fail. % Backtrack to find more characters
@@ -224,11 +358,12 @@ find_characters_with_role(_). % End of search
 
 % Predicate to search all information about a character
 search_character_info(Character) :-
-    character(Character, Role, Crew, DevilFruits),
+    character(Character, Role, Crew, DevilFruits,Bounty),
     write('Name: '), writeln(Character),
     write('Role: '), writeln(Role),
     write('Crew: '), writeln(Crew),
-    write('Devil Fruits: '), writeln(DevilFruits).
+    write('Devil Fruits: '), writeln(DevilFruits),
+    write('Bounty: '), writeln(Bounty).
 
 % Entry point
 start :-
